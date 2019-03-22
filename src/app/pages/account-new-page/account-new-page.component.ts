df3ab457fabe29e83199820ad8c1aadda37128ad
Asiapenguin <nicholaskong@live.ca>
2019-03-22 00:16:10 -0700

    Update account form, styles changes, refactored forms again

--- src/app/pages/account-new-page/account-new-page.component.ts
+++ src/app/pages/account-new-page/account-new-page.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 import { RouteService } from "src/app/services/route.service";
+import { Account } from "src/app/models/account";
+import { Pokemon } from "src/app/models/pokemon";
+import { AccountService } from "src/app/services/account.service";
+import { PokemonService } from "src/app/services/pokemon.service";
 
-
+export class SignUpData {
+  public username: string;
+  public password: string;
+  public characterName: string;
+  public gender: string;
+  public starterChoice: number;
+}
 
 @Component({
   selector: 'app-account-new-page',
@@ -9,8 +19,31 @@ import { RouteService } from "src/app/services/route.service";
   styleUrls: ['./account-new-page.component.scss']
 })
 export class AccountNewPageComponent {
+  signUpData: SignUpData = new SignUpData();
+  account: Account = new Account();
+  pokemon: Pokemon = new Pokemon();
 
-  constructor(private routeService: RouteService) { }
+  constructor(private accountService: AccountService, private pokemonService: PokemonService, private routeService: RouteService) { }
+
+  doSignUp() {
+    // POST /account
+    this.accountService.create(this.account).then((data: Account) => {
+      this.account = data;
+      console.log("After create user: ", this.account);
+      this.pokemon.ownerID = this.account.id;
+      // POST /pokemon
+      this.pokemonService.create(this.pokemon).then((data: Pokemon) => {
+        this.pokemon = data;
+        console.log("After create pokemon: ", this.pokemon);
+      },
+      err => {
+        console.log(err);
+      });
+    },
+    err => {
+      console.log(err);
+    });
+  }
 
   goBack() {
     this.routeService.goToLogin();
--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from "src/app/models/account";
+import { Pokemon } from "src/app/models/pokemon";
+import { AccountService } from "src/app/services/account.service";
+import { PokemonService } from "src/app/services/pokemon.service";
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  newAccount: Account = new Account();
+  editAccount: Account = new Account();
+  pokemon: Pokemon = new Pokemon();
+  oldAccId: number;
+
+  constructor(private accountService: AccountService, private pokemonService: PokemonService) { }
 
   ngOnInit() {
+  }
+
+  createAccount() {
+    // POST /account
+    this.accountService.create(this.newAccount).then((data: Account) => {
+      this.newAccount = data;
+      console.log("After create account: ", this.newAccount);
+      this.pokemon.ownerID = this.newAccount.id;
+      // POST /pokemon
+      this.pokemonService.create(this.pokemon).then((data: Pokemon) => {
+        this.pokemon = data;
+        console.log("After create pokemon: ", this.pokemon);
+      },
+      err => {
+        console.log(err);
+      });
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  updateAccount() {
+    // PUT: /account
+    this.accountService.create(this.editAccount).then((data: Account) => {
+      this.editAccount = data;
+      console.log("After update account: ", this.editAccount);
+    },
+    err => {
+      console.log(err);
+    });
+  }
+
+  setStarter(pokedexNumber: number) {
+    this.pokemon.dexNum = pokedexNumber;
   }
 
 }
