6031fc24baf67670dc62edf75bd37093710b16c5
Asiapenguin <nicholaskong@live.ca>
2019-03-27 16:25:16 -0700

    Changes to Digletts Cave, and sizing for account pokemon component, added README for installation instructions

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Pokemon Let's Go 304
 
-UBC CPSC 304 Term Project: Front-end Repository
+UBC CPSC 304 Term Project: Front-end Repository and General Installation Instructions
+
+Back-end Repository: [Link](https://github.com/kelseylaw/pokemon-lets-go-three-o-four-backend);
 
 ## Team
 
@@ -14,15 +16,64 @@ UBC CPSC 304 Term Project: Front-end Repository
 
 ## Development Notes
 
-### Setup & Run Dev Server
+### Setup & Run
+
+#### Database
+Make sure you have downloaded PostgreSQL for your environment (Mac/Windows/Linux) and remember your superuser password
+
+Start PostgreSQL (Path is default in this example for Windows with version 11)
+```bash
+$ pg_ctl -D "C:/Program Files/PostgreSQL/11/data" start
+```
+For Mac users, use homebrew and start PostgreSQL service.
+
+Connect to *postgres* as *postgres* superuser and enter your superuser password
+```bash
+$ psql postgres postgres
+```
+Create database named *proj304*
+```
+$ CREATE DATABASE proj304;
+```
+Create user named *three* with password *p304*
+```
+$ CREATE ROLE three WITH LOGIN PASSWORD 'p304';
+```
+Logout of PostgreSQL server and using *data.sql* in the root directory of backend repository, populate the *proj304* database with user *three* and enter *p304* as password
+```
+$ psql --username=three proj304 < data.sql
+```
+A bunch of SQL queries should have ran. If so, the *proj304* database has been populated successfully!
+
+#### Front-end code
+Make sure you have cloned and in the root directory of this repository (pokemon-lets-go-three-o-four-dashboard)
 
 ```bash
 $ npm install     # install dependencies
 $ ng serve --open # run development server and open on default browser
 ```
+
+#### Back-end code
+Make sure you have cloned the backend repository and in the root directory of that repository (pokemon-lets-go-three-o-four-backend)
+
+```bash
+$ npm install     # install dependencies
+$ node index.js   # run backend server that connects to PostgreSQL database
+```
+
 #### Notes
 
-If you run into issues, maybe try this
+If you run into issues while running front-end code, maybe try this
 ```
 $ npm rebuild node-sass
+```
+
+If you run into localhost database and backend service connection issues, please consider changing this file: *pg_hba.conf*
+Replace section with title "Allow replication connections from localhost, by a user with the replication privilege"
+Note: This is for Windows users, not sure if Mac/Linux users have this issue.
+```
+host    replication     all             127.0.0.1/32            trust
+host    replication     all             ::1/128                 trust
+host	all				all 			0.0.0.0/0				trust
+host	all				all 			::/0					trust
 ```
--- src/app/components/catch-pokemon/catch-pokemon.component.ts
+++ src/app/components/catch-pokemon/catch-pokemon.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
 import { PokemonService } from "src/app/services/pokemon.service";
 import { MapRegion } from "src/app/models/mapRegion";
 import { ListResponse } from "src/app/services/resource.service";
 import { Species } from "src/app/models/species";
 import { SpeciesService } from "src/app/services/species.service";
-import { HttpClient } from "@angular/common/http";
-import { UrlService } from "src/app/services/url.service";
-import { AuthenticationService } from "src/app/services/authentication.service";
 import { Account } from "src/app/models/account";
-import { ItemType } from "src/app/models/itemType";
 import { Pokemon } from "src/app/models/pokemon";
-import { ItemTypeCount } from "src/app/models/itemTypeCount";
 import { AccountService } from "src/app/services/account.service";
 import { ItemService } from "src/app/services/item.service";
 
@@ -19,7 +14,7 @@ import { ItemService } from "src/app/services/item.service";
   templateUrl: "./catch-pokemon.component.html",
   styleUrls: ["./catch-pokemon.component.scss"]
 })
-export class CatchPokemonComponent implements OnInit {
+export class CatchPokemonComponent implements OnInit, OnChanges {
   @Input() currentMap: MapRegion;
   @Input() currentAccount: Account;
   species = [];
@@ -32,20 +27,37 @@ export class CatchPokemonComponent implements OnInit {
     private accountService: AccountService,
     private pokemonService: PokemonService,
     private itemService: ItemService
-  ) {}
+  ) { }
 
   ngOnInit() {
-    this.speciesService
-      .findWhere("foundAt", this.currentMap.name)
-      .get()
-      .then((data: ListResponse<Species>) => {
-        console.log("CatchPokemonComponent: ", data);
-        this.species = data.data;
-      });
-
+    this.getCurrentSpecies().then((data: Species[]) => {
+      this.species = data;
+    });
     this.accountService.getAccountItemTypeCounts(this.currentAccount.id).then(data => {
       this.currentItemTypeCounts = data;
     });
+  }
+
+  ngOnChanges() {
+    this.getCurrentSpecies().then((data: Species[]) => {
+      this.species = data;
+    });
+  }
+
+  getCurrentSpecies() {
+    return new Promise((res, rej) => {
+      this.speciesService
+        .findWhere("foundAt", this.currentMap.name)
+        .get()
+        .then((data: ListResponse<Species>) => {
+          console.log("CatchPokemonComponent Current MapRegion's Species: ", data);
+          res(data.data);
+        },
+        err => {
+          console.log("CatchPokemonComponent GET /species/search?foundAt= error: ", err);
+          rej(err);
+        });
+    })
   }
 
   getSpeciesIconSrc(name: string) {
@@ -65,13 +77,13 @@ export class CatchPokemonComponent implements OnInit {
     });
 
     if (Math.random() > 0.5) {
-      this.result = `Pokemon with Pokedex number ${randomPokemonDexNum} has been caught!`;
       const pokemon = new Pokemon();
       pokemon.ownerId = this.currentAccount.id;
       pokemon.dexNum = randomPokemonDexNum;
       // POST /pokemon
       this.pokemonService.create(pokemon).then(
-        data => {
+        (data: Pokemon) => {
+          this.result = `${data.name} has been caught!`;
           console.log("Pokemon caught: ", data);
         },
         err => {
