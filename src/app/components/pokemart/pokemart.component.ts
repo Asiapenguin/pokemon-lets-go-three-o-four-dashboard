c0a0d4bcb1db16e854de52b91e852db9a9b2e127
Asiapenguin <nicholaskong@live.ca>
2019-03-28 21:56:54 -0700

    Remove multi quantity purchase in pokemart and finished sell log

--- src/app/components/pokemart/pokemart.component.ts
+++ src/app/components/pokemart/pokemart.component.ts
@@ -4,10 +4,11 @@ import { ItemTypeService } from "src/app/services/itemtype.service";
 import { ItemType } from "src/app/models/itemType";
 import { ListResponse } from "src/app/services/resource.service";
 import { AccountService } from "src/app/services/account.service";
-import { ItemPurchase } from "./sale-item/sale-item.component";
 import { Item } from "src/app/models/item";
 import { ItemService } from "src/app/services/item.service";
 import { SellLogService } from "src/app/services/sell-log.service";
+import { MapRegion } from 'src/app/models/mapRegion';
+import { SellLog } from 'src/app/models/sellLog';
 
 @Component({
   selector: "app-pokemart",
@@ -16,6 +17,7 @@ import { SellLogService } from "src/app/services/sell-log.service";
 })
 export class PokemartComponent implements OnInit {
   @Input() currentAccount: Account;
+  @Input() currentMap: MapRegion;
   @Output() newBalance = new EventEmitter<number>();
   saleItemTypes = [];
 
@@ -40,9 +42,8 @@ export class PokemartComponent implements OnInit {
       );
   }
 
-  purchase(itemPurchase: ItemPurchase) {
-    const cost = itemPurchase.itemType.cost * itemPurchase.quantity;
-    const newBalance = this.currentAccount.balance - cost;
+  purchase(itemType: ItemType) {
+    const newBalance = this.currentAccount.balance - itemType.cost;
 
     const editAccount = this.currentAccount;
     editAccount.balance = newBalance;
@@ -63,19 +64,16 @@ export class PokemartComponent implements OnInit {
       }
     );
 
-    const promiseArr = [];
-    for (let i = 0; i < itemPurchase.quantity; i++) {
       const newItem = new Item();
       newItem.playableId = this.currentAccount.id;
-      newItem.type = itemPurchase.itemType.type;
+      newItem.type = itemType.type;
+      
       // POST: /item
-      promiseArr.push(this.itemService.create(newItem));
-    }
-
-    Promise.all(promiseArr).then(
-      data => {
+      this.itemService.create(newItem).then(
+      (data: Item) => {
+        this.createSellLog(this.currentAccount.id, data.id);
         console.log(
-          `All items have been created for Account ID ${
+          `Item of type ${data.type} have been created for Account ID ${
             editAccount.id
           }: ${data}`
         );
@@ -84,5 +82,27 @@ export class PokemartComponent implements OnInit {
         console.log("PokemartComponent Promise.all POST /item error: ", err);
       }
     );
+  }
+
+  createSellLog(playableId: number, itemId: number) {
+    const pokemartBuildingId = this.currentMap.buildings
+      .filter(b => b.type === "PokeMart")
+      .map(b => b.id);
+    const newSellLog = new SellLog();
+    newSellLog.buildingid = pokemartBuildingId[0];
+    newSellLog.playableid = playableId;
+    newSellLog.itemid = itemId;
+    this.sellLogService.create(newSellLog).then((data: SellLog) => {
+      console.log(
+        `Sell log created for Account with ID ${
+          data.playableid
+        } at Building with ID ${
+          data.buildingid
+        }`
+      );
+    },
+    err => {
+      console.log("PokemartComponent POST /sell failed: ", err);
+    });
   }
 }
--- src/app/components/pokemart/sale-item/sale-item.component.ts
+++ src/app/components/pokemart/sale-item/sale-item.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ItemType } from 'src/app/models/itemType';
 
-export class ItemPurchase {
-  public itemType: ItemType;
-  public quantity: number;
-}
 
 @Component({
   selector: 'app-sale-item, [app-sale-item]',
@@ -15,16 +11,16 @@ export class SaleItemComponent implements OnInit {
 
   @Input() saleItemType: ItemType;
   @Input() balance: number;
-  @Output() purchase = new EventEmitter<ItemPurchase>();
+  @Output() purchase = new EventEmitter<ItemType>();
 
-  currentQuantity: number;
+  // currentQuantity: number;
   haveEnoughBalance = true;
   saleItemSrc = "";
 
   constructor() { }
 
   ngOnInit() {
-    this.currentQuantity = 0;
+    // this.currentQuantity = 0;
     this.setSaleItemSrc(this.saleItemType.type);
   }
 
@@ -32,28 +28,24 @@ export class SaleItemComponent implements OnInit {
     this.saleItemSrc = `../../../assets/item-sprites/${name.replace(" ", "-")}.png`;
   }
 
-  decrementQuantity() {
-    if (this.currentQuantity > 0) {
-      this.currentQuantity -= 1;
-      this.checkBalance();
-    }
-  }
+  // decrementQuantity() {
+  //   if (this.currentQuantity > 0) {
+  //     this.currentQuantity -= 1;
+  //     this.checkBalance();
+  //   }
+  // }
 
-  incrementQuantity() {
-    this.currentQuantity += 1;
-    this.checkBalance();
-  }
+  // incrementQuantity() {
+  //   this.currentQuantity += 1;
+  //   this.checkBalance();
+  // }
 
   private checkBalance() {
-    this.haveEnoughBalance = this.balance >= this.currentQuantity * this.saleItemType.cost;
+    // this.haveEnoughBalance = this.balance >= this.currentQuantity * this.saleItemType.cost;
+    this.haveEnoughBalance = this.balance >= this.saleItemType.cost;
   }
 
   makePurchase() {
-    const itemPurchase = new ItemPurchase();
-    itemPurchase.itemType = this.saleItemType;
-    itemPurchase.quantity = this.currentQuantity;
-    if (itemPurchase.quantity > 0) {
-      this.purchase.emit(itemPurchase);
-    }
+    this.purchase.emit(this.saleItemType);
   }
 }
